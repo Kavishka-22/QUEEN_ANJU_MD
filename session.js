772abb6a66b40f3c6ebeab6a7cacedd0ad296496
Kavishka-22 <Yasirukavishka01@gmail.com>
2024-11-28 08:56:53 +0530

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=SAcmFCCA#VQdOKhuEyt2AO5M4_vxLFa1f6iGUx2p2UQ6FMH-uSBQ",
 };
